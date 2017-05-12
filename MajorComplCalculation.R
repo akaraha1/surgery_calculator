2cb082f17829ed71ad42d94a7366ee65ea347575
Andrew Rosenblum <arosen35@jhu.edu>
2017-05-12 13:34:10 -0400

    Initial commit

---
+++ MajorComplCalculation.R
@@ -0,0 +1,33 @@
+
+CalcMajorRisk <- function() {
+  
+  ###Calculate the Major Complication Risk
+  anyComplRaw <- 0.00  #Make sure we're starting from 0
+  anyComplRaw <- majorComp_sexFactor*dfMaster[1,'Sex']
+  anyComplRaw <- anyComplRaw + majorComp_raceFactor*dfMaster[1,'Race']
+  anyComplRaw <- anyComplRaw + majorComp_ageFactor*dfMaster[1, 'Age']
+  #anyComplRaw <- anyComplRaw + dfMaster[1, 'Surgery']
+  anyComplRaw <- anyComplRaw + switch(as.character(dfMaster[1, 'Surgery']),
+                                      "Pancreas" = 0,
+                                      "Stomach" = majorComp_GastRxnFactor,
+                                      "Colon" = majorComp_ColonRxnFactor, 0)
+  anyComplRaw <- anyComplRaw + majorComp_FunctionalFactor*dfMaster[1, 'Funcational']
+  anyComplRaw <- anyComplRaw + majorComp_CancerGIFactor*dfMaster[1, 'Cancer']
+  anyComplRaw <- anyComplRaw + majorComp_asaclassFactor*dfMaster[1, 'ASAClass']
+  anyComplRaw <- anyComplRaw + majorComp_steroidFactor*dfMaster[1, 'Steroid']
+  anyComplRaw <- anyComplRaw + majorComp_ascitesFactor*dfMaster[1, 'Ascites']
+  anyComplRaw <- anyComplRaw + majorComp_SepticFactor*dfMaster[1, 'Septic']
+  anyComplRaw <- anyComplRaw + majorComp_ventilarFactor*dfMaster[1, 'Vent']
+  anyComplRaw <- anyComplRaw + majorComp_DMallFactor*dfMaster[1, 'DMAll']
+  anyComplRaw <- anyComplRaw + majorComp_hypermedFactor*dfMaster[1, 'HTNMed']
+  anyComplRaw <- anyComplRaw + majorComp_hxchfFactor*dfMaster[1, 'HxCHF']
+  anyComplRaw <- anyComplRaw + majorComp_SOBFactor*dfMaster[1, 'SOB']
+  anyComplRaw <- anyComplRaw + majorComp_smokerFactor*dfMaster[1, 'Smoker']
+  anyComplRaw <- anyComplRaw + majorComp_hxcopdFactor*dfMaster[1, 'HxCOPD']
+  anyComplRaw <- anyComplRaw + majorComp_dialysisFactor*dfMaster[1, 'Dialysis']
+  anyComplRaw <- anyComplRaw + majorComp_renafailFactor*dfMaster[1, 'RenalFailure']
+  anyComplRaw <- anyComplRaw + majorComp_BMIFactor*dfMaster[1, 'BMI']
+  return(anyComplRaw + majorComp_consFactor)
+  
+  
+}
