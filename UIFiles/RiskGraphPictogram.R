a90598113f781f405e4eebdeaa05af49b719b380
Andrew Rosenblum <arosen35@jhu.edu>
2017-05-19 12:13:41 -0400

    Minor UI changes and fixed the image not loading.

--- UIFiles/RiskGraphPictogram.R
+++ UIFiles/RiskGraphPictogram.R
@@ -5,17 +5,17 @@ library(grid)
 fill_images <- function() {
   withProgress(message = 'Building The Graphs', value = 0, {
     
-  l <- list()
-  for (i in 1:nrow(df3)) 
-  {
-    for (j in 1:ceiling(df3$units[i]))
+    l <- list()
+    for (i in 1:nrow(df3)) 
     {
-      incProgress(i/nrow(df3))
-      img <- readPNG("Heart_symbol_c00.png")
-      g <- rasterGrob(img, interpolate=TRUE)
-      l <- c(l, annotation_custom(g, xmin = i-1/2, xmax = i+1/2, ymin = j-1, ymax = j))
+      for (j in 1:ceiling(df3$units[i]))
+      {
+        incProgress(i/nrow(df3))
+        img <- readPNG("www/Heart_symbol_c00.png")
+        g <- rasterGrob(img, interpolate=TRUE)
+        l <- c(l, annotation_custom(g, xmin = i-1/2, xmax = i+1/2, ymin = j-1, ymax = j))
+      }
     }
-  }
   })
   l
 }
@@ -43,7 +43,7 @@ major_grid <- 0:10 * 2
 p <- ggplot(df3, aes(what, units)) + 
   fill_images() + 
   clip_images() +
-  geom_bar(fill=NA, colour="darkgreen", size=1.2, alpha=0.5, stat="identity") + 
+  geom_bar(fill=NA, colour="transparent", size=1.2, alpha=0.5, stat="identity") + 
   coord_flip() + 
   scale_y_continuous(breaks=seq(0, 20, 2)) + 
   scale_x_discrete() + 
