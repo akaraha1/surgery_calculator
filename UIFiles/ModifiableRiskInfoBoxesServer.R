b74357cec53124f63ed94bf6d0970fd8c92a79fc
Andrew Rosenblum <arosen35@jhu.edu>
2017-06-07 15:27:42 -0400

    Minor changes to reflect the new sever updates.

--- UIFiles/ModifiableRiskInfoBoxesServer.R
+++ UIFiles/ModifiableRiskInfoBoxesServer.R
@@ -17,13 +17,10 @@ withProgress(message = 'Calculting Modifiable Risk', value = 0, {
 
 ##Show functional Status Box if applicable
 output$FunctStatusBox <- renderUI({
-  if(dfMaster[1, 'Funcational'] < 2) {#if the person is less than fully independent
-    tmpRisk <- dfMaster[1,'Raw_MajorComplications'] - majorComp_FunctionalFactor*dfMaster[1, 'Funcational'] + majorComp_FunctionalFactor*2
-    newRisk <- expMajorRisk(tmpRisk) - expMajorRisk(dfMaster[1,'Raw_MajorComplications']) 
-    
+  if(dfRiskChanges[,'Funcational'] != -1) {#if the person is less than fully independent
     valueBox(
-      paste0("+", formatC(newRisk*-1, digits = 1, format = "f"), "%"),
-      "Functional Status Contribution",
+      paste0("+", formatC(dfRiskChanges[,'Funcational']*-1, digits = 1, format = "f"), "%"),
+      "Functional Status",
       icon = icon("list"),
       color = "red"
     )
@@ -33,10 +30,9 @@ incProgress(1/8)
 
 ##Show Sterioid Status Box if applicable
 output$SteroidBox <- renderUI({
-  if(dfMaster[1, 'Steroid'] == 1) {#if steroids == yes
-    riskChange <- dfMaster[1,'MajorComplications'] - expMajorRisk(dfMaster[1,'Raw_MajorComplications'] - majorComp_steroidFactor)
+  if(dfRiskChanges[,'Steroid'] != -1) {#if steroids == yes
     valueBox(
-      paste0("+", formatC(riskChange, digits = 1, format = "f"), "%"),
+      paste0("+", formatC(dfRiskChanges[,'Steroid'], digits = 1, format = "f"), "%"),
       "Steroid Risk Contribution",
       icon = icon("list"),
       color = "red"
@@ -46,13 +42,11 @@ output$SteroidBox <- renderUI({
 
 incProgress(2/8)
 
-
 ##Show CHF Status Box if applicable
 output$CHFBox <- renderUI({
   if(dfMaster[1, 'HxCHF'] == 1) {#if CHF status == yes
-    riskChange <- dfMaster[1,'MajorComplications'] - expMajorRisk(dfMaster[1,'Raw_MajorComplications'] - majorComp_hxchfFactor)
     valueBox(
-      paste0("+", formatC(riskChange, digits = 1, format = "f"), "%"),
+      paste0("+", formatC(dfRiskChanges[,'HxCHF'], digits = 1, format = "f"), "%"),
       "CHF Risk Contribution",
       icon = icon("list"),
       color = "red"
@@ -62,13 +56,11 @@ output$CHFBox <- renderUI({
 
 incProgress(3/8)
 
-
 ##Show SOB Status Box if applicable
 output$SOBBox <- renderUI({
   if(dfMaster[1, 'SOB'] == 1) {#if SOB status == yes
-    riskChange <- dfMaster[1,'MajorComplications'] - expMajorRisk(dfMaster[1,'Raw_MajorComplications'] - majorComp_SOBFactor)
     valueBox(
-      paste0("+", formatC(riskChange, digits = 1, format = "f"), "%"),
+      paste0("+", formatC(dfRiskChanges[,'SOB'], digits = 1, format = "f"), "%"),
       "SOB Risk Contribution",
       icon = icon("list"),
       color = "red"
@@ -78,13 +70,11 @@ output$SOBBox <- renderUI({
 
 incProgress(4/8)
 
-
 ##Show COPD Status Box if applicable
-output$COPDBox <- renderUI({
+output$COPDBox1 <- renderUI({
   if(dfMaster[1, 'HxCOPD'] == 1) {#if COPD status == yes
-    riskChange <- dfMaster[1,'MajorComplications'] - expMajorRisk(dfMaster[1,'Raw_MajorComplications'] - majorComp_hxcopdFactor)
     valueBox(
-      paste0("+", formatC(riskChange, digits = 1, format = "f"), "%"),
+      paste0("+", formatC(dfRiskChanges[,'HxCOPD'], digits = 1, format = "f"), "%"),
       "COPD Risk Contribution",
       icon = icon("list"),
       color = "red"
@@ -97,9 +87,8 @@ incProgress(5/8)
 ##Show Smoker Status Box if applicable
 output$smokerBox <- renderUI({
   if(dfMaster[1, 'Smoker'] == 1) {#if Smoking status == yes
-    riskChange <- dfMaster[1,'MajorComplications'] - expMajorRisk(dfMaster[1,'Raw_MajorComplications'] - majorComp_smokerFactor)
-    valueBox(
-      paste0("+", formatC(riskChange, digits = 1, format = "f"), "%"),
+     valueBox(
+      paste0("+", formatC(dfRiskChanges[,'Smoker'], digits = 1, format = "f"), "%"),
       "Smoking Risk Contribution",
       icon = icon("list"),
       color = "red"
@@ -112,9 +101,8 @@ incProgress(6/8)
 ##Show Diabetic Status Box if applicable
 output$DMBox <- renderUI({
   if(dfMaster[1, 'DMAll'] == 1) {#if diabetic status == yes
-    riskChange <- dfMaster[1,'MajorComplications'] - expMajorRisk(dfMaster[1,'Raw_MajorComplications'] - majorComp_DMallFactor)
     valueBox(
-      paste0("+", formatC(riskChange, digits = 1, format = "f"), "%"),
+      paste0("+", formatC(dfRiskChanges[,'DMAll'], digits = 1, format = "f"), "%"),
       "Diabetic Risk Contribution",
       icon = icon("list"),
       color = "red"
@@ -127,15 +115,28 @@ incProgress(7/8)
 ##Show HTN Status Box if applicable
 output$HTNBox <- renderUI({
   if(dfMaster[1, 'HTNMed'] == 1) {#if Smoking status == yes
-    riskChange <- dfMaster[1,'MajorComplications'] - expMajorRisk(dfMaster[1,'Raw_MajorComplications'] - majorComp_hypermedFactor)
     valueBox(
-      paste0("+", formatC(riskChange, digits = 1, format = "f"), "%"),
-      "Hypertension Risk Contribution",
+      paste0("+", formatC(dfRiskChanges[,'HTNMed'], digits = 1, format = "f"), "%"),
+      "Hypertension Risk",
       icon = icon("list"),
       color = "red"
     )
   }
 })
+
+output$BMIBOX <- renderGvis({
+  if(dfMaster[1, 'BMI'] > 25) {
+    gvisGauge(data.frame(Item='BMI',Value=dfMaster[1, 'BMI']),
+              options=list(min=0,
+                           max=55,
+                           greenFrom=15,
+                           greenTo=25,
+                           yellowFrom=25,
+                           yellowTo=35,
+                           redFrom=35, redTo=55)
+              )
+    }
+  })
 
 })
 
